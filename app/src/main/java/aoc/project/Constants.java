315f24e94a55f12e42c0945ac939dfce518cc73b
Fafnyer <maxcstone1@gmail.com>
2023-12-01 09:18:13 -0600

    changed session id and path

--- app/src/main/java/aoc/project/Constants.java
+++ app/src/main/java/aoc/project/Constants.java
@@ -2,10 +2,10 @@ package aoc.project;
 
 public class Constants {
     // This needs to be set to your machine's setup of the project.
-    public final static String PATH_TO_PROJECT = "C:\\Users\\maxcs\\Documents\\Robotics\\AoC-project\\AoC-Project";
+    public final static String PATH_TO_PROJECT = "D:\\Documents\\AdventOfCOde";
 
     // Extract from browser session; only good for 30 days then will need a new value from the browser
-    public final static String SESSION_ID = "session=53616c7465645f5ff773e5e0be1253b7dea2f06e8dc2e58840313e895e3fbb926b06f8bbc3ea1c9ab914908f0e6c5ea06af049bb4b79cd59139f40df89d772f8";
+    public final static String SESSION_ID = "session=53616c7465645f5f5ef5984f278fb64d2ae762b1330bee1cbf6c8d1fb1074a92ff45a8d179b7851fdb176515e920df5a14d0fe84e7d49187f65e8535f0dc9299";
 
     // This does change often so made it a constant
     public final static int AOC_YEAR = 2022
