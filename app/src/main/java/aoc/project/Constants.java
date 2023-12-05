15b0a90794f145d3fa1063d666e40b525d2629ac
Fafnyer <maxcstone1@gmail.com>
2023-12-05 10:00:49 -0600

    changed path

--- app/src/main/java/aoc/project/Constants.java
+++ app/src/main/java/aoc/project/Constants.java
@@ -2,7 +2,7 @@ package aoc.project;
 
 public class Constants {
     // This needs to be set to your machine's setup of the project.
-    public final static String PATH_TO_PROJECT = "D:\\Documents\\AdventOfCOde";
+    public final static String PATH_TO_PROJECT = "D:\\Documents\\AdventOfCOde\\AOC-Project";
 
     // Extract from browser session; only good for 30 days then will need a new value from the browser
     public final static String SESSION_ID = "session=53616c7465645f5f5ef5984f278fb64d2ae762b1330bee1cbf6c8d1fb1074a92ff45a8d179b7851fdb176515e920df5a14d0fe84e7d49187f65e8535f0dc9299";
