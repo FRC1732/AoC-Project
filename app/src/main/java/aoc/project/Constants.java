abe1451d1901cdbb33abd54fa5248caef4f725a0
Theodore Kranz <kranzt17@muhs.edu>
2024-08-30 10:54:55 -0500

    basic setup

--- app/src/main/java/aoc/project/Constants.java
+++ app/src/main/java/aoc/project/Constants.java
@@ -2,11 +2,11 @@ package aoc.project;
 
 public class Constants {
     // This needs to be set to your machine's setup of the project.
-    public final static String PATH_TO_PROJECT = "";
+    public final static String PATH_TO_PROJECT = "C:\\Project\\AOC\\AoC-Project";
 
     // Extract from browser session; only good for 30 days then will need a new value from the browser
-    public final static String SESSION_ID = "";
+    public final static String SESSION_ID = "session=53616c7465645f5f33b53803657ec4d805af115e92d8db2d169f294d7780e575fdd2898bdd8e9d7c02bed76aeac78810857c51d57c3b406cad164e5351bbeda2";
 
     // This does change often so made it a constant
-    public final static int AOC_YEAR = 2022;
+    public final static int AOC_YEAR = 2023;
 }
