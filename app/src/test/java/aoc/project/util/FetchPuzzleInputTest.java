23992d7f2d465f3e8d4930381ec291837f17f68a
Theodore Kranz <kranzt17@muhs.edu>
2024-10-08 12:38:54 -0500

    day  8

--- app/src/test/java/aoc/project/util/FetchPuzzleInputTest.java
+++ app/src/test/java/aoc/project/util/FetchPuzzleInputTest.java
@@ -20,7 +20,7 @@ public class FetchPuzzleInputTest {
 
     @Test
     public void generateFiles_givenPathYearAndDay_generateNewFilesIfNoneExist(){
-        fetchPuzzleInput.fetchPuzzleInput(Constants.AOC_YEAR, 7);
+        fetchPuzzleInput.fetchPuzzleInput(Constants.AOC_YEAR, 8);
 
         assertTrue(true, "Nothing to see here.");
     }
