c3f57bdb7473350aab8726d1b4feb9459a0ecfb2
Nolan Lautz <lautzn2@muhs.edu>
2025-01-07 20:06:38 -0600

    Test

--- app/src/main/java/aoc/project/Constants.java
+++ app/src/main/java/aoc/project/Constants.java
@@ -2,11 +2,11 @@ package aoc.project;
 
 public class Constants {
     // This needs to be set to your machine's setup of the project.
-    public final static String PATH_TO_PROJECT = "";
+    public final static String PATH_TO_PROJECT = "/Users/nolanlautz/Documents/Robotics Team/AoC-Project";
 
     // Extract from browser session; only good for 30 days then will need a new value from the browser
-    public final static String SESSION_ID = "";
+    public final static String SESSION_ID = "session=53616c7465645f5f505e240e82624f1993d4f7377413394f12f2c293adcf33b06ee180ab5107f3a32c469ac72763e76866d1082c321d6c4eaad4179775e9da4d";
 
     // This does change often so made it a constant
-    public final static int AOC_YEAR = 2022;
+    public final static int AOC_YEAR = 2024;
 }
